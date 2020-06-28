a356362a9b9887225cc1a6f784466b9b8f353340
Nam <50193793+rizuki17@users.noreply.github.com>
2020-06-27 17:04:47 -0700

    MeterManagment and Test Scene

---
+++ eJam/Assets/Scripts/script_MeterManagement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class script_MeterManagement : MonoBehaviour
+{
+    Image image_meter;
+    private float float_maxMeter = 1f;
+    private static float float_currentMeter = 0.4f;
+    private static float float_timer = 0.0f;
+    private static float float_waitTime = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image_meter = GetComponent<Image>();
+        image_meter.fillAmount = float_currentMeter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float_timer += Time.deltaTime;
+        if (float_timer >= float_waitTime)
+        {
+            float_currentMeter -= 0.01f;
+            image_meter.fillAmount = float_currentMeter/float_maxMeter;
+            float_timer = 0;
+        }
+    }
+}
