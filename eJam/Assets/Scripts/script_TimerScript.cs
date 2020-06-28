ed120f21de5ea90afcd0b2d5b1d367b8a0afa268
rygreen <rygreen99@gmail.com>
2020-06-28 03:31:07 -0700

    Timer Script
    :^)

---
+++ eJam/Assets/Scripts/script_TimerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class script_TimerScript : MonoBehaviour
+{
+    public Text text_UIText;
+
+    private float float_timer;
+    private bool bool_counting;
+
+    void Start()
+    {
+        float_timer = 0;
+        bool_counting = false;
+        text_UIText.text = "0";
+    }
+
+    void Update()
+    {
+        if(bool_counting)
+        {
+            float_timer += Time.deltaTime;
+            int temp_int_numberForText = (int) float_timer;
+            text_UIText.text = "" + temp_int_numberForText;
+        }
+    }
+
+    public void StartTimer()
+    {
+        bool_counting = true;
+    }
+    public void EndTimer()
+    {
+        float_timer = 0;
+        bool_counting = false;
+        text_UIText.text = "0";
+    }
+}
