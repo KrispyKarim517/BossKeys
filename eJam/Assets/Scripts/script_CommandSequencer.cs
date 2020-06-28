a7c8ebb6c83495642baada4ec5a8cd1ac8b49f51
JosefBertolini <39634656+JosefBertolini@users.noreply.github.com>
2020-06-27 20:11:34 -0700

    added joke generation

--- eJam/Assets/Scripts/script_CommandSequencer.cs
+++ eJam/Assets/Scripts/script_CommandSequencer.cs
@@ -1,15 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class script_CommandSequencer : MonoBehaviour
 {
-    private char char_CommandCounter = 'A';
+    private List<string> list_AllJokes = new List<string>()
+                                        {
+                                            "STEAK PUNS ARE PRETTY RARE",
+                                            "INSULTING MY NOODLES IS PRETTY LO MEIN",
+                                            "THE RIGHT SAUCE IS TERIYA KEY", //Does this one make sense?
+                                            "I FRIED THIS RICE NOT THE SHRIMP",
+                                            "CHICKEN DOESNT NEED SEASONING",
+                                            "THIS CHICKEN CROSSED THE WRONG ROAD",
+                                        };
+    private List<string> list_OnlyNewJokes;
+
+    private Queue<string> queue_Commands = new Queue<string>();
+    
+    //private char char_CommandCounter = 'A';
+    private System.Random random_RandomIndexGenerator = new System.Random();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //while (char_CommandCounter != 'Z')
+        //   PushCommand(string.Format("COMMAND {0}", char_CommandCounter++));
+
+        list_OnlyNewJokes =  list_AllJokes.ToList();
     }
 
     // Update is called once per frame
@@ -20,6 +39,27 @@ public class script_CommandSequencer : MonoBehaviour
 
     public string GetNextCommand()
     {
-        return string.Format("COMMAND {0}", char_CommandCounter++); ;
+        if (queue_Commands.Count != 0)
+            return queue_Commands.Dequeue();
+        else
+        {
+            return GenerateJoke();
+        }
+    }
+
+    public void PushCommand(string str_new_command)
+    {
+        queue_Commands.Enqueue(str_new_command);
+    }
+
+    private string GenerateJoke()
+    {
+        if (list_OnlyNewJokes.Count == 0)
+            list_OnlyNewJokes = list_AllJokes.ToList();
+        
+        int int_random_index = random_RandomIndexGenerator.Next(list_OnlyNewJokes.Count);
+        string str_new_joke = list_OnlyNewJokes[int_random_index];
+        list_OnlyNewJokes.RemoveAt(int_random_index);
+        return str_new_joke;
     }
 }
