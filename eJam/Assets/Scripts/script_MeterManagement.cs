241bafeaaefc9bf63bd0eda15817f71015330d88
JosefBertolini <39634656+JosefBertolini@users.noreply.github.com>
2020-07-01 02:00:56 -0700

    burnt food gives no points

--- eJam/Assets/Scripts/script_MeterManagement.cs
+++ eJam/Assets/Scripts/script_MeterManagement.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 
 public class script_MeterManagement : MonoBehaviour
 {
+    [Header("Food Objects")]
+    public script_FoodScript ref_Food = null;
+
     Image image_meter;
     private float float_maxMeter = 1f;
-    private static float float_currentMeter = 0.55f;
+    private static float float_currentMeter = 0.4f;
     private static float float_timer = 0.0f;
     private static float float_waitTime = 1.0f;
 
+    //private const float float_PointValue = 0.075f;
     private string[] arr_str_SupportedCommands = { "SEASON", "GET", "GRILL", "GRAB", "SERVE" };
     
     // Start is called before the first frame update
@@ -38,7 +42,10 @@ public class script_MeterManagement : MonoBehaviour
         string str_command_first_word = bonus.Split()[0];
         if (arr_str_SupportedCommands.Contains(str_command_first_word))
         {
-            float_currentMeter += .075f;
+            float float_temp_points = 0.075f;
+            if (str_command_first_word == "SERVE")
+                float_temp_points = ref_Food.isBurnt() ? 0f : 0.075f;
+            float_currentMeter += float_temp_points;
             if (float_currentMeter > 1f)
                 float_currentMeter = 1f;
         }
